127dbade69277986aa3a67f0ec28ca23c9c05310
miko <miko3k@gmail.com>
2021-12-31 00:20:29 +0100

    JSON conversion

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Fred Jehle's Conjugated Spanish Verb Database
+# Improved version of Fred Jehle's Conjugated Spanish Verb Database
 
-PostgreSQL and CSV databases of 600+ conjugated Spanish verbs forming 11,000+ combinations of moods and tenses. Original work by Fred Jehle and database compiled by @ghidinelli. 
+PostgreSQL and CSV databases of 600+ conjugated Spanish verbs forming 11,000+ combinations of moods and tenses. Original work by Fred Jehle and database orignally compiled by @ghidinelli. 
 
-Read more: http://www.ghidinelli.com/free-spanish-conjugated-verb-database
+This repository is a total convertsion into JSON format. It also fixes plenty of issues and adds some additional information from the original source. Also contains one file per verb.
 
-This database has been incorporated into a number of tools including:
+The database is currently provided in JSON format only. I wouldn't be against an automated tool to convert it to different formats.
+
+The original database has been incorporated into a number of tools including:
 
  * Conjug - a free practice tool by @dcomtois is online - http://es.conjug.com
  * es-api - a free web API for accessing the database by @lwm - https://github.com/lwm/es-api
---
+++ lib/schema.ts
@@ -0,0 +1,67 @@
+export interface Forms {
+    yo: string;
+    tu: string;
+    ud: string;
+    nosotros: string;
+    vosotros: string;	
+    uds: string;
+}
+
+export interface ImperativoForms {
+    tu: string;
+    vosotros: string;	
+    ud: string;
+    uds: string;
+}
+
+export type Indicativo<T> = {
+    presente: T;
+    futuro: T;
+    imperfecto: T;
+    preterito: T;
+    condicional: T;
+    presentePerfecto: T;	
+    futuroPerfecto: T;
+    pluscuamperfecto: T;
+    preteritoAnterior?: T;
+    condicionalPerfecto: T;
+}
+
+export type Subjuntivo<T> = {
+    presente: T;
+    imperfecto: T;
+    futuro?: T;
+    presentePerfecto: T;
+    futuroPerfecto?: T;
+    pluscuamperfecto: T;
+}
+
+export type SubjuntivoAlt<T> = {
+    imperfectoAlt: T
+    pluscuamperfectoAlt: T
+}
+
+export type English = {
+    note: string;
+    indicativo: Indicativo<string>
+    subjuntivo: Subjuntivo<string>
+    infinitivo: string;
+    afirmativo: string;
+    negativo: string;
+    gerundio: string;
+    participioPasado: string;
+}
+
+
+export type Verb = {
+    regular: boolean
+    indicativo: Indicativo<Forms>
+    subjuntivo: Subjuntivo<Forms> & SubjuntivoAlt<Forms>
+    infinitivo: string;
+    afirmativo: ImperativoForms;
+    negativo: ImperativoForms;
+    gerundio: string;
+    participioPasado: string;
+    english?: English;
+    attribution: string;
+}
